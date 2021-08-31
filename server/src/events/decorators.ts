a79e2fda71e351515e4368dae601c13ff385f6b6
xshady <fastik036@gmail.com>
2021-08-31 19:58:44 +0300

    server: add defineOnClientTyped

--- server/src/events/decorators.ts
+++ server/src/events/decorators.ts
@@ -1,15 +1,20 @@
 import * as alt from "alt-server"
 import { defineTypedEventDecorator } from "@xshady/altv-decorators-shared"
 import type { AddPlayerParamToIEvents } from "./types"
+import type { RegisterEventHandler } from "@xshady/altv-decorators-shared/dist/events/types"
 
 export const onTyped = defineTypedEventDecorator("onTyped", (originalMethod, eventName) => {
   alt.on(eventName, originalMethod)
 })
 
+export const defineOnClientTyped = <T>(decoratorName: string, registerEvent: RegisterEventHandler<string>) => 
+  defineTypedEventDecorator(decoratorName, registerEvent)<AddPlayerParamToIEvents<T>>()
+
 export const onClientTyped = <T>() => 
-  defineTypedEventDecorator("onClientTyped", (originalMethod, eventName) => {
+  defineOnClientTyped<T>('onClientTyped', (originalMethod, eventName) => {
     alt.onClient(eventName, originalMethod)
-  })<AddPlayerParamToIEvents<T>>()
+  })
+
 
 export const onAlt = defineTypedEventDecorator("onAlt", (originalMethod, eventName) => {
   alt.onClient(eventName, originalMethod)
