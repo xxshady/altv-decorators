be52b3a69cbb9ecf9d172e340acaac406b8e38d5
xshady <fastik036@gmail.com>
2021-08-31 19:26:07 +0300

    add @onAlt

--- client/src/events/decorators.ts
+++ client/src/events/decorators.ts
@@ -1,10 +1,14 @@
 import * as alt from "alt-client"
 import { defineTypedEventDecorator } from "@xshady/altv-decorators-shared"
 
-export const onTyped = defineTypedEventDecorator("test", (originalMethod, eventName) => {
+export const onTyped = defineTypedEventDecorator("onTyped", (originalMethod, eventName) => {
   alt.on(eventName, originalMethod)
 })
 
-export const onServerTyped = defineTypedEventDecorator("test", (originalMethod, eventName) => {
+export const onServerTyped = defineTypedEventDecorator("onTyped", (originalMethod, eventName) => {
   alt.onServer(eventName, originalMethod)
 })
+
+export const onAlt = defineTypedEventDecorator("onAlt", (originalMethod, eventName) => {
+  alt.on(eventName, originalMethod)
+})<alt.IClientEvent>()
--- server/src/events/decorators.ts
+++ server/src/events/decorators.ts
@@ -1,10 +1,14 @@
 import * as alt from "alt-server"
 import { defineTypedEventDecorator } from "@xshady/altv-decorators-shared"
 
-export const onTyped = defineTypedEventDecorator("test", (originalMethod, eventName) => {
+export const onTyped = defineTypedEventDecorator("onTyped", (originalMethod, eventName) => {
   alt.on(eventName, originalMethod)
 })
 
-export const onClientTyped = defineTypedEventDecorator("test", (originalMethod, eventName) => {
+export const onClientTyped = defineTypedEventDecorator("onClientTyped", (originalMethod, eventName) => {
   alt.onClient(eventName, originalMethod)
 })
+
+export const onAlt = defineTypedEventDecorator("onAlt", (originalMethod, eventName) => {
+  alt.onClient(eventName, originalMethod)
+})<alt.IServerEvent>()
