bc19816b1f26a82d31484587dfd16d0546b88505
xshady <fastik036@gmail.com>
2021-08-31 19:36:29 +0300

    base event decorator return type of method any

--- shared/src/events/typed-event-decorator.ts
+++ shared/src/events/typed-event-decorator.ts
@@ -1,9 +1,9 @@
 import { defineMethodDecorator } from "../base-method-decorator"
-import type { OriginalMethodHandler } from "../types"
-
-type DecoratorEventArgs<T> = (eventName: T) => void
-type OriginalMethod<T extends (...args: any[]) => void> = (...args: Parameters<T>) => unknown
-type RegisterEventHandler<T> = OriginalMethodHandler<DecoratorEventArgs<T>>
+import type {
+  RegisterEventHandler,
+  DecoratorEventArgs,
+  OriginalMethod
+} from "./types"
 
 export const defineTypedEventDecorator = (decoratorName: string, registerEvent: RegisterEventHandler<string>) =>
   <T extends Record<any, any>>() =>
